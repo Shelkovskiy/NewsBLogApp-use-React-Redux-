2c9f20ec1ada9f8920b5bd038edc5cd83be9d9ca
SErgey Shelkovskiye <kadetlepel@mail.ru>
2022-09-29 17:16:46 +0300

    add default props for reuse comp

--- react-redux-app/src/components/common-components/Form/index.ts
+++ react-redux-app/src/components/common-components/Form/index.ts
@@ -5,16 +5,32 @@ interface IFormProps {
 	maxheigth: string;
 	padding: string;
 	margin: string;
+	flexDirection?: string;
+	width?: string;
 }
 
 export const Form = styled.form<IFormProps>`
+	width: ${(props) => props.width || "auto"};
 	border-radius: 16px;
 	display: flex;
-	flex-direction: column;
+	flex-direction: ${(props) => props.flexDirection || "column"};
 	box-sizing: border-box;
 	max-width: ${(props) => props.maxwidth}px;
 	max-height: ${(props) => props.maxheigth}px;
 	padding: ${(props) => props.padding}px;
 	margin: ${(props) => props.margin};
 	box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+`;
+
+export const FormLabel = styled.label`
+	font-size: 16px;
+	font-family: "Inter";
+	font-weight: 600;
+	line-height: 20px;
+	color: #313037;
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	font-weight: 600;
+	margin-bottom: 24px;
 `;
