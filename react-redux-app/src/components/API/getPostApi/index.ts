02d39dfcaefe5f293b5789ba6e4e517e8f080a6b
SErgey Shelkovskiye <kadetlepel@mail.ru>
2022-10-05 13:30:46 +0300

    add news async reques for BLogs and news

--- react-redux-app/src/components/API/getPostApi/index.ts
+++ react-redux-app/src/components/API/getPostApi/index.ts
@@ -1,7 +1,20 @@
 import { URL_ARTICLES } from "../../constants";
+import { URL_BLOGS } from "../../constants";
 
 export const getPostsArr = async () => {
 	const PostsAPIrequesJSON = await fetch(URL_ARTICLES);
+	const response = await PostsAPIrequesJSON.json();
+	return response;
+};
+
+export const getNewsArr = async () => {
+	const PostsAPIrequesJSON = await fetch(URL_BLOGS);
+	const response = await PostsAPIrequesJSON.json();
+	return response;
+};
+
+export const getSignglePosts = async (id:string) => {
+	const PostsAPIrequesJSON = await fetch(`https://api.spaceflightnewsapi.net/v3/articles/${id}`);
 	const response = await PostsAPIrequesJSON.json();
 	return response;
 };
