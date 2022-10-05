1339f36deedc5899e34370137d0ce8173e272adc
SErgey Shelkovskiye <kadetlepel@mail.ru>
2022-10-05 13:29:34 +0300

    add constants for BLog and add new page

--- react-redux-app/src/App.tsx
+++ react-redux-app/src/App.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/common-components/Layout/index";
 import { MainPage } from "./pages/MainPage";
+import NewsPage from "./pages/NewsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import Searchpage from "./pages/SearchPage";
+import SelectedPage from "./pages/SelectedPage";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
 
@@ -13,10 +15,12 @@ const App = () => {
 			<Route element={<Layout />}>
 				<Route path="/" element={<Navigate replace to="/mainpage" />} />
 				<Route path="/mainpage" element={<MainPage />} />
+				<Route path="/newspage" element={<NewsPage />} />
 				<Route path="/searchpage" element={<Searchpage />} />
 				<Route path="/signinpage" element={<SignInPage />} />
 				<Route path="/signupage" element={<SignUpPage />} />
 				<Route path="*" element={<NotFoundPage />} />
+				<Route path="/selectedpage/:id" element={<SelectedPage />} />
 			</Route>
 		</Routes>
 	);
