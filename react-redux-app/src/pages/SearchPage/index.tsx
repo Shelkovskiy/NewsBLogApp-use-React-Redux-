19ea7ed8adc4b7bf2b70fcf03f818f71bd34b622
SErgey Shelkovskiye <kadetlepel@mail.ru>
2022-10-05 13:26:05 +0300

    ne smotri,eto podgotovil stranicy dlya poiska

--- react-redux-app/src/pages/SearchPage/index.tsx
+++ react-redux-app/src/pages/SearchPage/index.tsx
@@ -1,5 +1,34 @@
 import React from "react";
-export const Searchpage = () => {
-	return <div className="container"></div>;
+import ComponentsContainer from "../../components/common-components/Container";
+import CustomText from "../../components/common-components/Text";
+import List from "../../components/common-components/UserList/List";
+import useState from 'react';
+
+const Searchpage = () => {
+	return (
+		<ComponentsContainer
+			maxWidth="1120px"
+			margin="60px auto 0"
+			padding="0px 0px 10px "
+		>
+			<ComponentsContainer maxWidth="100%" margin="0px 0px 64px">
+				<CustomText
+					fontsize="56"
+					lineheight="80"
+					fontweight="700"
+					color="#313037"
+					width="734px"
+					margin="start"
+				>
+					Search results
+				</CustomText>
+			</ComponentsContainer>
+			<ComponentsContainer>
+				<div>
+					{/* <List items={filterBlogs} /> */}
+				</div>
+			</ComponentsContainer>
+		</ComponentsContainer>
+	);
 };
 export default Searchpage;
