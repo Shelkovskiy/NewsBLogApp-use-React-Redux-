4ac9ca58ce3b54881d6604397c897ecfdf6507a9
SErgey Shelkovskiye <kadetlepel@mail.ru>
2022-10-10 20:05:54 +0300

    rename file

---
+++ react-redux-app/src/components/constants/URLS/index.ts
@@ -0,0 +1,3 @@
+export const URL_POSTS = "https://jsonplaceholder.typicode.com/posts";
+export const URL_ARTICLES = "https://api.spaceflightnewsapi.net/v3/articles";
+export const URL_BLOGS = "https://api.spaceflightnewsapi.net/v3/blogs";
