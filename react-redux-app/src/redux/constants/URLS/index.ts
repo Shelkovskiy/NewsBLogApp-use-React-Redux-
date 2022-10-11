c26d47442ff717f586eb35c6ff5adfe86ad57cc7
SErgey Shelkovskiye <kadetlepel@mail.ru>
2022-10-11 16:19:19 +0300

    add new URL

--- react-redux-app/src/redux/constants/URLS/index.ts
+++ react-redux-app/src/redux/constants/URLS/index.ts
@@ -1,4 +1,6 @@
 export const URL_POSTS = "https://jsonplaceholder.typicode.com/posts";
-export const URL_NEWS = "https://api.spaceflightnewsapi.net/v3/articles";
-export const URL_BLOGS = `https://api.spaceflightnewsapi.net/v3/blogs`;
-export const URL_BLOG_COUNT = "https://api.spaceflightnewsapi.net/v3/articles/count"
+export const BASE_BLOG_URL = "https://api.spaceflightnewsapi.net";
+export const URL_ARTICLES = `${BASE_BLOG_URL}/v3/articles?_limit=12&_start={currentPage}`;
+export const URL_ARTICLES_COUNT = `${BASE_BLOG_URL}/v3/articles/count`;
+export const URL_NEWS = `${BASE_BLOG_URL}/v3/blogs`;
+
