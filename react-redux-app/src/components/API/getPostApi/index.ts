eb17c304bb546341b33ca4675e00f3d57fe7dc7b
Sergey Shelkovskiy <kadetlepel@mail.ru>
2022-10-20 18:38:27 +0300

    used base api

--- react-redux-app/src/components/API/getPostApi/index.ts
+++ react-redux-app/src/components/API/getPostApi/index.ts
@@ -1,9 +1,7 @@
+import { BASE_BLOG_URL } from "../../../redux/constants/URLS";
+
 export const getSignglePosts = async (id: string) => {
-	const PostsAPIrequesJSON = await fetch(
-		`https://api.spaceflightnewsapi.net/v3/articles/${id}`,
-	);
-	const response = await PostsAPIrequesJSON.json();
+	const postsAPIrequesJSON = await fetch(`${BASE_BLOG_URL}/v3/articles/${id}`);
+	const response = await postsAPIrequesJSON.json();
 	return response;
 };
-
-export {};
