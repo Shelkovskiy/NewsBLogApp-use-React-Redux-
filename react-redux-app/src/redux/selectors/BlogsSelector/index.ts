58d846cc47e03ca89de5d2711e33f9b381db4368
Sergey Shelkovskiy <kadetlepel@mail.ru>
2022-10-22 17:27:22 +0300

    fix workplace

--- react-redux-app/src/redux/selectors/BlogsSelector/index.ts
+++ react-redux-app/src/redux/selectors/BlogsSelector/index.ts
@@ -1,9 +1,10 @@
 import { RootState } from "../../store";
 
-export const blogSelectors = (state:RootState) => state.blogs.blogs;
-export const isLoadingSelector =(state:RootState) =>state.blogs.isLoading;
-export const errorSelector = (state:RootState) => state.blogs.error;
-export const currentPageSelector =(state:RootState) =>state.blogs.currentPage;
-export const perPageSelector =(state:RootState) =>state.blogs.perPage;
-export const totalCountSelector =(state:RootState) =>state.blogs.totalCount;
-export const searchResult =(state:RootState) =>state.blogs.searchblogs;
+export const blogSelectors = (state: RootState) => state.blogs.blogs;
+export const isLoadingSelector = (state: RootState) => state.blogs.isLoading;
+export const errorSelector = (state: RootState) => state.blogs.error;
+export const currentPageSelector = (state: RootState) =>
+	state.blogs.currentPage;
+export const perPageSelector = (state: RootState) => state.blogs.perPage;
+export const totalCountSelector = (state: RootState) => state.blogs.totalCount;
+export const searchResult = (state: RootState) => state.blogs.searchblogs;
