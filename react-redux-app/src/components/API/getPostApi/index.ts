2757f3a889de01a4bc045428de90632ef24da4c3
Sergey Shelkovskiy <kadetlepel@mail.ru>
2022-10-22 13:35:20 +0300

    refactor app wood ,del useless files

--- react-redux-app/src/components/API/getPostApi/index.ts
+++
@@ -1,7 +0,0 @@
-import { BASE_BLOG_URL } from "../../../redux/constants/URLS";
-
-export const getSignglePosts = async (id: string) => {
-	const postsAPIrequesJSON = await fetch(`${BASE_BLOG_URL}/v3/articles/${id}`);
-	const response = await postsAPIrequesJSON.json();
-	return response;
-};
