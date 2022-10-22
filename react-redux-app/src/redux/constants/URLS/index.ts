ec15672b5ad8dc141c956a54589e95f464249ba1
Sergey Shelkovskiy <kadetlepel@mail.ru>
2022-10-22 17:28:29 +0300

    fix urls

--- react-redux-app/src/redux/constants/URLS/index.ts
+++ react-redux-app/src/redux/constants/URLS/index.ts
@@ -1,5 +1,3 @@
 export const URL_POSTS = "https://jsonplaceholder.typicode.com/posts";
 export const BASE_BLOG_URL = "https://api.spaceflightnewsapi.net";
-export const URL_ARTICLES = `${BASE_BLOG_URL}/v3/articles?_limit=12&_start={currentPage}`;
-export const URL_ARTICLES_COUNT = `${BASE_BLOG_URL}/v3/articles/count`;
-export const URL_NEWS = `${BASE_BLOG_URL}/v3/blogs`;
+
