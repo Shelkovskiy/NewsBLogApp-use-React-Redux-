dbfda9a54dc559fd902bdb6270aa78055d165422
Sergey Shelkovskiy <kadetlepel@mail.ru>
2022-10-26 12:50:44 +0300

    fix workplace

--- react-redux-app/src/redux/store/index.ts
+++ react-redux-app/src/redux/store/index.ts
@@ -18,3 +18,8 @@ export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export const store = configureStore({});
+
+type PropertiesType<T> = T extends { [key: string]: infer U } ? U : never;
+export type InferActionTypes<
+	T extends { [key: string]: (...args: any[]) => any },
+> = ReturnType<PropertiesType<T>>;
