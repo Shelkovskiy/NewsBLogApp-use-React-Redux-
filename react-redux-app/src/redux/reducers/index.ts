8ecd1a4d25c7e808d6b7630744939569591ffbf4
Sergey Shelkovskiy <kadetlepel@mail.ru>
2022-10-26 12:52:46 +0300

    add authreducer

--- react-redux-app/src/redux/reducers/index.ts
+++ react-redux-app/src/redux/reducers/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { blogReducer } from "./blogsReducer";
 import { newsReducer } from "./newsReducer/index";
+import authReducer from "./authReducer";
 
 const rootReducer = combineReducers({
 	blogs: blogReducer,
 	news: newsReducer,
+	auth: authReducer,
 });
 export default rootReducer;
