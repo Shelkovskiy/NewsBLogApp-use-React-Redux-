22edca755de0f8123e27920f4a6ab40a6f8e0b31
Sergey Shelkovskiy <kadetlepel@mail.ru>
2022-10-26 12:41:32 +0300

    add new page for project

--- react-redux-app/src/App.tsx
+++ react-redux-app/src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/common-components/Layout/index";
+import PersistLogin from "./components/PersistLogin";
+import ActivationPage from "./pages/ActivationPage";
 import MainPage from "./pages/MainPage";
 import NewsPage from "./pages/NewsPage";
 import NotFoundPage from "./pages/NotFoundPage";
@@ -13,14 +15,17 @@ const App = () => {
 	return (
 		<Routes>
 			<Route element={<Layout />}>
-				<Route path="/" element={<Navigate replace to="/mainpage" />} />
-				<Route path="/mainpage" element={<MainPage />} />
-				<Route path="/newspage" element={<NewsPage />} />
-				<Route path="/searchpage" element={<Searchpage />} />
+				<Route path="/" element={<Navigate replace to="/signinpage" />} />
 				<Route path="/signinpage" element={<SignInPage />} />
 				<Route path="/signupage" element={<SignUpPage />} />
 				<Route path="*" element={<NotFoundPage />} />
+				<Route path="/activate/:uid/:id" element={<ActivationPage />} />
 				<Route path="/selectedpage/:id" element={<SelectedPage />} />
+				<Route element={<PersistLogin />}>
+					<Route path="/mainpage" element={<MainPage />} />
+					<Route path="/newspage" element={<NewsPage />} />
+					<Route path="/searchpage" element={<Searchpage />} />
+				</Route>
 			</Route>
 		</Routes>
 	);
