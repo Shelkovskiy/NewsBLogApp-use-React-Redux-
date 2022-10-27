97c85625dba58f68da080070f4893c89f1dd4c57
Sergey Shelkovskiy <kadetlepel@mail.ru>
2022-10-27 19:05:39 +0300

    style:fix workplace,refactor:fix pagination

--- react-redux-app/src/components/Pagination/createPagesFUnc/index.ts
+++ react-redux-app/src/components/Pagination/createPagesFUnc/index.ts
@@ -1,25 +1,67 @@
-interface IPages {
-	pages: number[];
-	pageCount: number;
+import { useMemo } from "react";
+
+interface IPagination {
+	totalCount: number;
+	pageSize: number;
+	siblingCount: number;
 	currentPage: number;
 }
 
-export function createPages({ pages, pageCount, currentPage }: IPages) {
-	if (pageCount > 12) {
-		if (currentPage > 5) {
-			for (let i = currentPage - 2; i <= currentPage + 5; i++) {
-				pages.push(i);
-				if (i === pageCount) break;
-			}
-		} else {
-			for (let i = 1; i <= 10; i++) {
-				pages.push(i);
-				if (i === pageCount) break;
-			}
+const DOTS: any = "...";
+
+const range = (start: number, end: number) => {
+	let length = end - start + 1;
+	return Array.from({ length }, (_, idx) => idx + start);
+};
+
+export const usePagination = ({
+	totalCount,
+	pageSize,
+	siblingCount = 1,
+	currentPage,
+}: IPagination) => {
+	const paginationRange = useMemo(() => {
+		const totalPageCount = Math.ceil(totalCount / pageSize);
+
+		const totalPageNumbers = siblingCount + 5;
+
+		if (totalPageNumbers >= totalPageCount) {
+			return range(1, totalPageCount);
 		}
-	} else {
-		for (let i = 1; i <= pageCount; i++) {
-			pages.push(i);
+
+		const leftSiblingIndex = Math.max(currentPage - siblingCount, 1);
+		const rightSiblingIndex = Math.min(
+			currentPage + siblingCount,
+			totalPageCount,
+		);
+
+		const shouldShowLeftDots = leftSiblingIndex > 2;
+		const shouldShowRightDots = rightSiblingIndex < totalPageCount - 2;
+
+		const firstPageIndex = 1;
+		const lastPageIndex = totalPageCount;
+
+		if (!shouldShowLeftDots && shouldShowRightDots) {
+			let leftItemCount = 3 + 2 * siblingCount;
+			let leftRange = range(1, leftItemCount);
+
+			return [...leftRange, DOTS, totalPageCount];
 		}
-	}
-}
+
+		if (shouldShowLeftDots && !shouldShowRightDots) {
+			let rightItemCount = 3 + 2 * siblingCount;
+			let rightRange = range(
+				totalPageCount - rightItemCount + 1,
+				totalPageCount,
+			);
+			return [firstPageIndex, DOTS, ...rightRange];
+		}
+
+		if (shouldShowLeftDots && shouldShowRightDots) {
+			let middleRange = range(leftSiblingIndex, rightSiblingIndex);
+			return [firstPageIndex, DOTS, ...middleRange, DOTS, lastPageIndex];
+		}
+	}, [totalCount, pageSize, siblingCount, currentPage]);
+
+	return paginationRange;
+};
--- react-redux-app/src/components/Pagination/index.ts
+++ react-redux-app/src/components/Pagination/index.ts
@@ -5,17 +5,17 @@ interface IPageProps {
 }
 
 export const Page = styled.span<IPageProps>`
-	width: 30px;
+	width: 40px;
 	font-family: "Inter";
 	font-style: normal;
 	font-weight: ${(props) => (props.isSelected ? "700" : "400")};
-	font-size: 16px;
+	font-size: 14px;
 	line-height: 24px;
 	text-transform: uppercase;
 	cursor: pointer;
 	border: ${(props) =>
 		props.isSelected ? "3px solid black" : "1px solid grey"};
-	border-radius: 50%;
+	border-radius: 60%;
 	text-align: center;
 	color: ${(props) => (props.isSelected ? "black" : "rgba(49, 48, 55, 1)")};
 `;
