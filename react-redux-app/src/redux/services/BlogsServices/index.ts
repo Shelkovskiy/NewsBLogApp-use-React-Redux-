651d88a09a93eae4c90d7892f4498987ea15942b
Sergey Shelkovskiy <kadetlepel@mail.ru>
2022-11-01 17:31:02 +0300

    refactor: use axioscontent

--- react-redux-app/src/redux/services/BlogsServices/index.ts
+++ react-redux-app/src/redux/services/BlogsServices/index.ts
@@ -1,33 +1,30 @@
-import axios from "axios";
-import { IAsyncBlogsResponseData } from "../../Types/responseType";
-import { BASE_BLOG_URL } from "../../constants/urls";
+import { IAsyncBlogsResponseData, ISinglePage } from "../../Types/responseType";
+import { axiosContent } from "../../../components/API";
 
 export const getAsyncBlogsFromApi = ({
 	currentPage = 1,
 	limit = 12,
 }: {
-	currentPage?: number;
+	currentPage: number | string;
 	limit?: number;
 }) => {
-	return axios.get<IAsyncBlogsResponseData[]>(
-		`${BASE_BLOG_URL}/v3/articles?_limit=${limit}&_start=${
-			limit * (currentPage - 1)
+	return axiosContent.get<IAsyncBlogsResponseData[]>(
+		`/v3/articles?_limit=${limit}&_start=${
+			limit * ((currentPage as number) - 1)
 		}`,
 	);
 };
 
 export const getAsyncBlogsCount = () => {
-	return axios.get<number>(`${BASE_BLOG_URL}/v3/articles/count`);
+	return axiosContent.get<number>(`/v3/articles/count`);
 };
 
 export const blogsSearch = ({ filter = "" }: { filter?: string }) => {
-	return axios.get<IAsyncBlogsResponseData[]>(
-		`${BASE_BLOG_URL}/v3/articles?title_contains=${filter}`,
+	return axiosContent.get<IAsyncBlogsResponseData[]>(
+		`/v3/articles?title_contains=${filter}`,
 	);
 };
 
 export const getSignglePosts = async (id: string) => {
-	const postsAPIrequesJSON = await fetch(`${BASE_BLOG_URL}/v3/articles/${id}`);
-	const response = await postsAPIrequesJSON.json();
-	return response;
+	return axiosContent.get<ISinglePage>(`/v3/articles/${id}`);
 };
